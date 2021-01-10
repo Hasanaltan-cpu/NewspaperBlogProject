d6b620255284f9c31a392537313878f70caee879
Hasan ALTAN <hsnaltan13.com>
2021-01-10 20:01:09 +0300

    Final Commit

---
+++ NewspaperBlogProject.UI/Areas/Member/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using NewspaperBlogProject.DataAccessLayer.Repositories.Concrete.EfRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewspaperBlogProject.UI.Areas.Member.Controllers
+{
+    public class HomeController : Controller
+    {
+        EfPostRepository _postRepository;
+
+        public HomeController() => _postRepository = new EfPostRepository();
+
+        // GET: Member/Home
+        public ActionResult Index()
+        {
+            return View(_postRepository.GetActive().OrderByDescending(x => x.CreateDate));
+        }
+    }
+}
---
+++ NewspaperBlogProject.UI/Areas/Member/Controllers/PostController.cs
@@ -0,0 +1,39 @@
+﻿using NewspaperBlogProject.DataAccessLayer.Repositories.Concrete.EfRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewspaperBlogProject.EntityLayer.Enums;
+using NewspaperBlogProject.UI.Areas.Member.Data.VMs;
+
+namespace NewspaperBlogProject.UI.Areas.Member.Controllers
+{
+    public class PostController : Controller
+    {
+        EfPostRepository _postRepo;
+        EfAppUserRepository _appUserRepo;
+        EfLikeRepository _likeRepo;
+        EfCommentRepository _commentRepo;
+
+        public PostController()
+        {
+            _postRepo = new EfPostRepository();
+            _appUserRepo = new EfAppUserRepository();
+            _likeRepo = new EfLikeRepository();
+            _commentRepo = new EfCommentRepository();
+        }
+
+        // GET: Member/Post
+        public ActionResult Show(int id)
+        {
+            PostDetailsVM data = new PostDetailsVM();
+            data.Post = _postRepo.GetById(id);
+            data.AppUser = _appUserRepo.GetById(data.Post.AppUserId);
+
+            data.Comments = _commentRepo.GetDefault(x => x.PostId == id && x.Status != Status.Passived);
+
+            return View();
+        }
+    }
+}
